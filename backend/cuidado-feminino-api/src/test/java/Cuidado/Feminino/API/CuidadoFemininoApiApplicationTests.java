5dcecd9a4592f8ff5afe13f2ded9f412efac293c
Antônio E. Wandrey <antonioewandrey.dev@gmail.com>
2026-03-19 22:19:58 -0300

    feat: estrutura base do monorepo com frontend limpo e backend inicial

---
+++ backend/cuidado-feminino-api/src/test/java/Cuidado/Feminino/API/CuidadoFemininoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Cuidado.Feminino.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidadoFemininoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
